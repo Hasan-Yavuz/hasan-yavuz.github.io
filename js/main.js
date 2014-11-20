5e073e701beca914e6419a02c438a691b283c958
Hasan-Yavuz <yavu0003@algonquinlive.com>
2014-11-19 22:09:41 -0500

    Changed nav li font family, added word mark

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
+var $wordmark = $('.wordmark');
+var $landing = $('.landing');
+
 $(document).ready(function() {
 	$(".fancybox").fancybox({
 		openEffect	: 'none',
 		closeEffect	: 'none'
 	});
+});
+
+$win.on('scroll', function () {
+	var scrollPos = $win.scrollTop();
+
+	$wordmark.css('transform', 'center ' + scrollPos / 20 + 'px');
+	$landing.css('background-position', 'center ' + scrollPos / 20 + 'px');
 });
